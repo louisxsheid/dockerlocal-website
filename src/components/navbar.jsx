7217ae42a73360e365c41ef51c8cd5a318852e4a
Louis Xavier Sheid III <louisxsheid@gmail.com>
2020-07-16 10:00:07 -0700

    remove boilerplate extras

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,49 +1,31 @@
 import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import logo from '../assets/whale_v2.0.png'
 import github from '../assets/githublogo.png'
-import 'bootstrap/dist/css/bootstrap.min.css';
 import Navbar from 'react-bootstrap/NavBar';
-import Nav from 'react-bootstrap/Nav';
-import Button from 'react-bootstrap/Button';
-import NavDropdown from 'react-bootstrap/NavDropdown';
-import Form from 'react-bootstrap/Form';
-import FormControl from 'react-bootstrap/FormControl';
+
 export default class Head extends React.Component {
-    render() {
-        return (
-            <div>
-                <Navbar bg="primary" expand="lg" variant="dark">
-                    <Navbar.Brand className='nav-wrap'>
-                        <div class='dockerlocal'>
-                        <img src={logo} className='logo-icon'></img>
-                        DockerLocal
-                        </div>
- 
-                    </Navbar.Brand>
-                    <Navbar.Toggle aria-controls="basic-navbar-nav" />
-                    <Navbar.Collapse id="basic-navbar-nav">
-                        <Nav className="mr-auto">
-                            {/* <Nav.Link href="#home">Home</Nav.Link>
-                            <Nav.Link href="#link">Link</Nav.Link>
-                            <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-                                <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-                                <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
-                                <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-                                <NavDropdown.Divider />
-                                <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
-                            </NavDropdown> */}
-                        </Nav>
-                    </Navbar.Collapse>
-                    <div className='dl-beta'>
-                        download beta
-                        <a href='https://github.com/oslabs-beta/dockerlocal' target="_blank">
-                            <img src={github} className='github-icon'></img>
+  render() {
+    return (
+      <div>
+        <Navbar bg="primary" expand="lg" variant="dark">
+          <Navbar.Brand className='nav-wrap'>
 
-                        </a>
-                        </div>
-                </Navbar>
-
+            <div class='dockerlocal'>
+              <img src={logo} className='logo-icon'></img>
+              DockerLocal
             </div>
-        );
-    }
+
+            <div className='dl-beta'>
+              download beta
+            <a href='https://github.com/oslabs-beta/dockerlocal' target="_blank">
+                <img src={github} className='github-icon'></img>
+              </a>
+            </div>
+            
+          </Navbar.Brand>
+        </Navbar>
+      </div>
+    );
+  }
 }
--- src/components/team.jsx
+++ src/components/team.jsx
@@ -5,48 +5,48 @@ import tom from '../assets/tom.png'
 import vivian from '../assets/vivian.jpeg'
 
 export default class Team extends React.Component {
-    render() {
-        return (
-            <div classNameName='App'>
-                <h1 class='abt-us'>About Us</h1>
-                <div className="grid-container">
-                    <div className="grid-item">
-                    <img className='profile-pic' src={louis}/>
-                        <h4>Louis Xavier Sheid III</h4>
-                        <a href='https://github.com/louisxsheid' target="_blank">
-                            <img src={github} className='github-icon-profile' ></img>
-                        </a>
-                    </div>
-                    <div className="grid-item">
-                        <img className='profile-pic' src="https://avatars1.githubusercontent.com/u/7851187?s=400&u=9ca2535660568ac197def8260d976ac9513fa9c4&v=4"></img>
-                        <h4>Katty Polyak</h4>
-                        <a href='https://github.com/kattypolyak' target="_blank">
-                            <img src={github} className='github-icon-profile' ></img>
-                        </a>
-                    </div>
-                    <div className="grid-item">
-                        <img className='profile-pic' src={tom}></img>
-                        <h4>Tom Lutz</h4>
-                        <a href='https://github.com/tlutz888' target="_blank">
-                            <img src={github} className='github-icon-profile' ></img>
-                        </a>
-                    </div>
-                    <div className="grid-item">
-                        <img className='profile-pic' src="https://avatars2.githubusercontent.com/u/64158930?s=400&u=95dd18f7989f9baaac6a899676c49371fa84307a&v=4"></img>
-                        <h4>Kate Chanthakaew</h4>
-                        <a href='https://github.com/katechantha' target="_blank">
-                            <img src={github} className='github-icon-profile' ></img>
-                        </a>
-                    </div>
-                    <div className="grid-item">
-                        <img className='profile-pic' src={vivian}></img>
-                        <h4>Vivian Cermeno</h4>
-                        <a href='https://github.com/vcermeno' target="_blank">
-                            <img src={github} className='github-icon-profile' ></img>
-                        </a>
-                    </div>
-                </div>
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div classNameName='App'>
+        <h1 class='abt-us'>About Us</h1>
+        <div className="grid-container">
+          <div className="grid-item">
+            <img className='profile-pic' src={louis} />
+            <h4>Louis Xavier Sheid III</h4>
+            <a href='https://github.com/louisxsheid' target="_blank">
+              <img src={github} className='github-icon-profile' ></img>
+            </a>
+          </div>
+          <div className="grid-item">
+            <img className='profile-pic' src="https://avatars1.githubusercontent.com/u/7851187?s=400&u=9ca2535660568ac197def8260d976ac9513fa9c4&v=4"></img>
+            <h4>Katty Polyak</h4>
+            <a href='https://github.com/kattypolyak' target="_blank">
+              <img src={github} className='github-icon-profile' ></img>
+            </a>
+          </div>
+          <div className="grid-item">
+            <img className='profile-pic' src={tom}></img>
+            <h4>Tom Lutz</h4>
+            <a href='https://github.com/tlutz888' target="_blank">
+              <img src={github} className='github-icon-profile' ></img>
+            </a>
+          </div>
+          <div className="grid-item">
+            <img className='profile-pic' src="https://avatars2.githubusercontent.com/u/64158930?s=400&u=95dd18f7989f9baaac6a899676c49371fa84307a&v=4"></img>
+            <h4>Kate Chanthakaew</h4>
+            <a href='https://github.com/katechantha' target="_blank">
+              <img src={github} className='github-icon-profile' ></img>
+            </a>
+          </div>
+          <div className="grid-item">
+            <img className='profile-pic' src={vivian}></img>
+            <h4>Vivian Cermeno</h4>
+            <a href='https://github.com/vcermeno' target="_blank">
+              <img src={github} className='github-icon-profile' ></img>
+            </a>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
